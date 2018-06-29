c346ea2f84307464cfba1b4288f3923af7b4f3d6
Canno <1985226606>
2018-06-29 14:55:06 +0800

    【update】socket server and client

---
+++ src/main/java/com/canno/blog/init/SocketServer.java
@@ -0,0 +1,38 @@
+package com.canno.blog.init;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author Canno
+ * @since 2018/6/28 17:06
+ */
+public class SocketServer {
+    public void init() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9001);
+        Socket socket = serverSocket.accept();
+        InputStream in = socket.getInputStream();
+        byte[] bytes = new byte[1024];
+        int count;
+        while (true) {
+            System.out.println("there has new request!");
+            while ((count = in.read(bytes)) != -1) {
+                System.out.println(new String(bytes));
+            }
+//            socket.getOutputStream().write("heiheihei".getBytes());
+            socket.shutdownInput();
+            OutputStream os = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+            pw.write("欢迎您！");
+            pw.flush();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new SocketServer().init();
+    }
+}
