9290e82ff9c62c49c2fa82993a48ba828c8456f3
Canno <1985226606>
2018-07-10 10:10:50 +0800

    【integration】spring shell

---
+++ src/main/java/com/canno/spring/boot/integration/shell/MyShell.java
@@ -0,0 +1,35 @@
+package com.canno.spring.boot.integration.shell;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+/**
+ * @author Canno
+ * @since 2018/7/9 19:03
+ */
+@ShellComponent
+public class MyShell {
+    @ShellMethod("Add two integers together.")
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @ShellMethod("Calculate two integers product.")
+    public int product(int a, int b) {
+        return a * b;
+    }
+
+    @ShellMethod("Zoo shout.")
+    public String zoo(String zooName) {
+        switch (zooName) {
+            case "cat":
+                return "喵喵喵";
+            case "dog":
+                return "汪汪汪";
+            default:
+                return "emmmmmmm";
+        }
+    }
+
+
+}
